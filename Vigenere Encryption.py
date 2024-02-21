c0c2218317f6c010946147650a36c12d50c51350
s0ur0u <96497609+s0ur0u@users.noreply.github.com>
2024-02-21 13:46:31 -0500

    Vigenere Encryption.py

--- Vigenere Encryption.py
+++ Vigenere Encryption.py
@@ -1,16 +1,16 @@
 text = 'Hello Zaira'
 custom_key = 'python'
 
-def vigenere(message, key):
+def vigenere(message, key, direction):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
     for char in message.lower():
     
         # Append space to the message
         if char == ' ':
-            encrypted_text += char
+            final_message += char
         else:        
             # Find the right key character to encode
             key_char = key[key_index % len(key)]
@@ -19,9 +19,11 @@ def vigenere(message, key):
             # Define the offset and the encrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
     
-    return encrypted_text
-encryption = vigenere(text, custom_key)
+    return final_message
+encryption = vigenere(text, custom_key, 1)
 print(encryption)
+decryption = vigenere(encryption, custom_key, -1)
+print(decryption)
